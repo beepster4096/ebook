cc63306c828a949456a47969917b00f8d75a8961
DrMeepster <19316085+DrMeepster@users.noreply.github.com>
2019-09-11 17:58:27 -0700

    Util

--- src/main/java/drmeepster/ebook/mixins/EnchantedBookItemMixin.java
+++ src/main/java/drmeepster/ebook/mixins/EnchantedBookItemMixin.java
@@ -2,7 +2,7 @@ package drmeepster.ebook.mixins;
 
 import org.spongepowered.asm.mixin.Mixin;
 
-import net.minecraft.enchantment.Enchantment;
+import drmeepster.ebook.Util;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.EnchantedBookItem;
@@ -11,8 +11,6 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.Tag;
-import net.minecraft.util.Identifier;
-import net.minecraft.util.registry.Registry;
 import net.minecraft.world.World;
 
 @Mixin(EnchantedBookItem.class)
@@ -22,7 +20,8 @@ public abstract class EnchantedBookItemMixin extends Item{
 		super(item$Settings_1);
 	}
 
-	private static final FoodComponent FOOD = (new FoodComponent.Builder()).hunger(2).saturationModifier(0).alwaysEdible().build();
+	private static final FoodComponent FOOD = 
+		new FoodComponent.Builder().hunger(2).saturationModifier(0).alwaysEdible().build();
 
 	@Override
 	public boolean isFood(){
@@ -33,37 +32,22 @@ public abstract class EnchantedBookItemMixin extends Item{
 	public FoodComponent getFoodComponent(){
 		return FOOD;
 	}
-	
+
 	@Override
 	public ItemStack finishUsing(ItemStack item, World world, LivingEntity entity){
 		int xp = 1;
-		
+
 		for(Tag tag : EnchantedBookItem.getEnchantmentTag(item)){
+			// Should always be true but can be modified with commands
 			if(tag instanceof CompoundTag){
-				CompoundTag enchantTag = (CompoundTag)tag;
-				
-				Enchantment enchantment = Registry.ENCHANTMENT.get(new Identifier(enchantTag.getString("id")));
-				
-				if(enchantment.isCursed()){
-					xp -= 1;
-				}else{
-					float lvl = enchantTag.getInt("lvl");
-					float maxLevel = enchantment.getMaximumLevel();
-					
-					//magic equation
-					//if lvl = 1 then 1 xp
-					//if lvl = maxLevel then 3 xp
-					//values in between are floored
-					
-					xp += (int)Math.min((2 / (maxLevel - 1)) * (lvl - 1) + 1, maxLevel);
-				}
+				xp += Util.getXP((CompoundTag) tag);
 			}
 		}
-		
+
 		if(entity instanceof PlayerEntity){
-			((PlayerEntity)entity).addExperience(xp);
+			((PlayerEntity) entity).addExperience(Math.min(xp, 0));
 		}
-		
+
 		return super.finishUsing(item, world, entity);
 	}
 }
